927fa658da5948e6888237255804586076c74eee
Eric Sales De Andrade <eric.salesdeandrade@joinstudyhall.com>
2023-08-08 12:56:38 +0530

    lower case

--- src/calculator.py
+++ src/calculator.py
@@ -2,25 +2,25 @@
 Python code to perform mathematical operations on two variable
 """
 
-def Summation(num1: int, num2: int) -> int:
+def summation(num1: int, num2: int) -> int:
     """
     Calculate the summation of two number
     """
     return num1 + num2
 
-def Subtraction(num1: int, num2: int) -> int:
+def subtraction(num1: int, num2: int) -> int:
     """
     Calculate the subtraction of two number
     """
     return num1 - num2
 
-def Multiplication(num1: int, num2: int) -> int:
+def multiplication(num1: int, num2: int) -> int:
     """
     Calculate the multiplication of two number
     """
     return num1 * num2
 
-def Division(num1: int, num2: int) -> float:
+def division(num1: int, num2: int) -> float:
     """
     Calculate the division of two number
     """
--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -1,38 +1,37 @@
-from src.calculator import (
-    Summation, 
-    Subtraction, 
-    Multiplication, 
-    Division
-)
+from src.calculator import summation, subtraction, multiplication, division
 
-def test_Summation():
+
+def test_summation():
     """
     Testing Summation function
     """
-    assert Summation(2, 10) == 12
-    assert Summation(3, 5) == 8
-    assert Summation(4, 6) == 10
+    assert summation(2, 10) == 12
+    assert summation(3, 5) == 8
+    assert summation(4, 6) == 10
 
-def test_Subtraction():
+
+def test_subtraction():
     """
     Testing Subtraction function
     """
-    assert Subtraction(8, 2) == 6
-    assert Subtraction(7, 5) == 2
-    assert Subtraction(4, 2) == 2
+    assert subtraction(8, 2) == 6
+    assert subtraction(7, 5) == 2
+    assert subtraction(4, 2) == 2
 
-def test_Multiplication():
+
+def test_multiplication():
     """
     Testing Multiplication function
     """
-    assert Multiplication(2, 2) == 4
-    assert Multiplication(7, 2) == 14
-    assert Multiplication(10, 2) == 20
+    assert multiplication(2, 2) == 4
+    assert multiplication(7, 2) == 14
+    assert multiplication(10, 2) == 20
 
-def test_Division():
+
+def test_division():
     """
     Testing Division function
     """
-    assert Division(5, 5) == 1
-    assert Division(70, 10) == 7
-    assert Division(16, 4) == 4
+    assert division(5, 5) == 1
+    assert division(70, 10) == 7
+    assert division(16, 4) == 4
